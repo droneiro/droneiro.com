9b1719ffbca31fb0889009ec13c6abc5b9f89ea1
kandebonfim <kandebonfim@gmail.com>
2017-11-23 00:56:18 -0200

    Update frontend code

--- source/assets/javascripts/registro.js
+++ source/assets/javascripts/registro.js
@@ -1,5 +1,9 @@
 $(".js-register-form").submit(function(e){
   e.preventDefault();
+  submitForm(e);
+});
+
+function submitForm(e){
   $.ajax({
     url: 'https://hooks.zapier.com/hooks/catch/602113/siw2ip/',
     type: 'get',
@@ -8,9 +12,13 @@ $(".js-register-form").submit(function(e){
     success: function(data) {
       document.body.classList.add('form-sent');
       console.log("Success!");
+    },
+    error: function(data) {
+      alert("Está tudo bem com a sua internet? Não estou conseguindo mandar os dados do seu formulário.");
+      submitForm(e);
     }
   });
-});
+}
 
 $(".js-register-form").conversationalForm({
   dictionaryData: {
@@ -19,9 +27,3 @@ $(".js-register-form").conversationalForm({
   }
 });
 
-
-$('input[name="nome"]').on('change', function() {
-  console.log($( this ).val());
-  console.log('changed');
-  // $('input[name="primeiro-nome"]')
-})

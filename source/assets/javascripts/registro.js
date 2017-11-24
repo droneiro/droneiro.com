d3fc5bf0671a4de25ff3b3cf9d67dad7ffa0c551
kandebonfim <kandebonfim@gmail.com>
2017-11-24 21:14:15 -0200

    Update form

--- source/assets/javascripts/registro.js
+++ source/assets/javascripts/registro.js
@@ -22,12 +22,98 @@ function submitForm(e){
 
 var dispatcher = new cf.EventDispatcher();
 dispatcher.addEventListener(cf.FlowEvents.USER_INPUT_UPDATE, function(event){
-  var serialized = $('.js-register-form').serializeArray().reduce(function(obj, item) {
+  var data = $('.js-register-form').serializeArray().reduce(function(obj, item) {
     obj[item.name] = item.value;
     return obj;
   }, {});
-  console.log(serialized);
+
+  $('.js-diagnóstico').attr('cf-questions', getDiagnostic(data));
 }, false);
+
+function getDiagnostic(data) {
+  var message = "",
+      userRegistros = [],
+      valorTotal = 29;
+
+  var registros = {
+    anatel: {
+      title: 'Anatel',
+      description: "A Anatel regula as radiofrequências no Brasil. Como os Drones e rádios possuem transmissores, precisamos homologar esses aparelhos com o órgão.",
+      instrucoes: "<a href='https://www.multidrones.com.br/2017/03/14/homologao-de-drones-pela-anatel-tudo-o-que-voc-precisa-saber/' target='_blank'>● Multidrones - Tudo o que você precisa saber sobre a homologação da Anatel</a><a href='http://www.anatel.gov.br/Portal/verificaDocumentos/documento.asp?numeroPublicacao=346061&pub=original&filtro=1&documentoPath=346061.pdf' target='_blank'>● Manual do Usuário Solicitante - Declaração de Conformidade para quadricóptero (DRONE)</a><br>",
+      sigla: "Agência Nacional de Telecomunicações",
+      valorOrgao: 200,
+      valor: 70
+    },
+    anac: {
+      title: 'Anac',
+      description: "A ANAC é o orgão que criou as regras para operações com drones. Precisamos cadastrar você e seu drone no sistema SISANT.",
+      instrucoes: "<a href='https://sistemas.anac.gov.br/SISANT/Operador/Cadastrar' target='_blank'>● Cadastro de Operador no SISANT</a><a href='http://www.anac.gov.br/assuntos/paginas-tematicas/drones/cadastro-de-drones' target='_blank'>● Cadastro de Drones - Informativo da Anac</a><a href='https://www.youtube.com/watch?v=SiP-BADS4qc' target='_blank'>● Como cadastrar drone na ANAC - SISANT 2017 — Video do canal Águias Drones</a><br>",
+      sigla: "Agência Nacional de Aviação Civil",
+      valorOrgao: 0,
+      valor: 25
+    },
+    decea: {
+      title: 'Decea',
+      description: "O DECEA disponibiliza um sistema para a solicitação de acesso ao espaço aéreo para o uso de drones. Precisamos cadastrar você e sua aeronave no SARPAS.",
+      instrucoes: "<a href='https://www.youtube.com/watch?v=AykL8kfcE4c' target='_blank'>● Como fazer o cadastro de usuário - Vídeo do canal do DECEA</a><a href='http://www.decea.gov.br/drone/' target='_blank'>● Portal Drone/RPAS - Decea</a><a href='http://servicos.decea.gov.br/sarpas/?i=cadastro' target='_blank'>● Novo cadastro no SARPAS</a><br>",
+      sigla: "Departamento de Controle do Espaço Aéreo",
+      valorOrgao: 0,
+      valor: 25
+    }
+  }
+
+  // Nome
+  if (data.nome){
+    message += "Beleza, "+data.nome+"! Com essas respostas eu já consigo te dizer o que você precisa!";
+  } else {
+    message += "Beleza! Com essas respostas eu já consigo te dizer quais registros estão faltando pra você voar tranquilo!";
+  }
+
+  // Anatel
+  if (data['selo-anatel'] == "não") {
+    userRegistros.push(registros.anatel);
+  }
+
+  // Anac
+  if (data.peso == "sim" && data.anac == "não") {
+    userRegistros.push(registros.anac);
+  }
+
+  // Decea
+  if (data.decea == "não") {
+    userRegistros.push(registros.decea);
+  }
+
+  if (userRegistros.length > 1) {
+    message += "&&São "+userRegistros.length+" registros ao todo:";
+    userRegistros.forEach( function(element, index) {
+      message += "<span><br><b>"+element.title+"</b> - "+element.description+"</span>";
+      valorTotal += element.valorOrgao;
+      valorTotal += element.valor;
+    });
+  } else if (userRegistros.length == 1){
+    message += "&&Na verdade, só falta um registro!";
+    message += "<span><br><b>"+userRegistros[0].title+"</b><br>"+userRegistros[0].description+"</span>";
+  } else {
+
+  }
+
+  message += "&&<span>Você tem duas opções: <br><br><b>● Deixar todo esse trabalho na nossa mão</b> por <b>R$ "+valorTotal+"</b>. Sem esquentar a cabeça com a burocracia do processo, você ainda vai receber na sua casa todos os documentos plastificados, adesivo do fabricantes e de identificação da aeronave em caso de perda por fly away. <br><br><b>● Fazer você mesmo</b> e arcar com os possíveis custos diretamente com os orgãos responsáveis.</span>";
+
+  setInstrucoes(data, userRegistros);
+
+  return message;
+}
+
+function setInstrucoes(data, userRegistros){
+  message = "Tranquilo, "+data.nome+"! Aqui vão as instruções pra sua aventura!&&<span>"
+  userRegistros.forEach( function(element, index) {
+    message += "<b>"+element.title+"</b>"+element.instrucoes;
+  });
+  message += "</span>"
+  message += "&&<span>Se você mudar de ideia e quiser deixar o serviço com a gente, é só clicar na sua resposta anterior.</span>"
+  $('.js-instrucoes').attr('cf-questions', message);
+}
 
 $(".js-register-form").conversationalForm({
   dictionaryData: {
@@ -38,7 +124,7 @@ $(".js-register-form").conversationalForm({
     controlElementsInAnimationDelay: 500,
     robot: {
       robotResponseTime: 0,
-      chainedResponseTime: 600
+      chainedResponseTime: 400
     }
   },
   hideUserInputOnNoneTextInput: true,
